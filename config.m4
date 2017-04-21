6d676fddd1ff9b288580c0c28df7b5843a3edc85
Laputa <shinemotec@gmail.com>
2017-04-21 12:22:37 +0800

    init project

---
+++ config.m4
@@ -0,0 +1,63 @@
+dnl $Id$
+dnl config.m4 for extension php_design_patterns
+
+dnl Comments in this file start with the string 'dnl'.
+dnl Remove where necessary. This file will not work
+dnl without editing.
+
+dnl If your extension references something external, use with:
+
+PHP_ARG_WITH(php_design_patterns, for php_design_patterns support,
+dnl Make sure that the comment is aligned:
+[  --with-php_design_patterns             Include php_design_patterns support])
+
+dnl Otherwise use enable:
+
+dnl PHP_ARG_ENABLE(php_design_patterns, whether to enable php_design_patterns support,
+dnl Make sure that the comment is aligned:
+dnl [  --enable-php_design_patterns           Enable php_design_patterns support])
+
+if test "$PHP_PHP_DESIGN_PATTERNS" != "no"; then
+  dnl Write more examples of tests here...
+
+  dnl # --with-php_design_patterns -> check with-path
+  dnl SEARCH_PATH="/usr/local /usr"     # you might want to change this
+  dnl SEARCH_FOR="/include/php_design_patterns.h"  # you most likely want to change this
+  dnl if test -r $PHP_PHP_DESIGN_PATTERNS/$SEARCH_FOR; then # path given as parameter
+  dnl   PHP_DESIGN_PATTERNS_DIR=$PHP_PHP_DESIGN_PATTERNS
+  dnl else # search default path list
+  dnl   AC_MSG_CHECKING([for php_design_patterns files in default path])
+  dnl   for i in $SEARCH_PATH ; do
+  dnl     if test -r $i/$SEARCH_FOR; then
+  dnl       PHP_DESIGN_PATTERNS_DIR=$i
+  dnl       AC_MSG_RESULT(found in $i)
+  dnl     fi
+  dnl   done
+  dnl fi
+  dnl
+  dnl if test -z "$PHP_DESIGN_PATTERNS_DIR"; then
+  dnl   AC_MSG_RESULT([not found])
+  dnl   AC_MSG_ERROR([Please reinstall the php_design_patterns distribution])
+  dnl fi
+
+  dnl # --with-php_design_patterns -> add include path
+  dnl PHP_ADD_INCLUDE($PHP_DESIGN_PATTERNS_DIR/include)
+
+  dnl # --with-php_design_patterns -> check for lib and symbol presence
+  dnl LIBNAME=php_design_patterns # you may want to change this
+  dnl LIBSYMBOL=php_design_patterns # you most likely want to change this 
+
+  dnl PHP_CHECK_LIBRARY($LIBNAME,$LIBSYMBOL,
+  dnl [
+  dnl   PHP_ADD_LIBRARY_WITH_PATH($LIBNAME, $PHP_DESIGN_PATTERNS_DIR/$PHP_LIBDIR, PHP_DESIGN_PATTERNS_SHARED_LIBADD)
+  dnl   AC_DEFINE(HAVE_PHP_DESIGN_PATTERNSLIB,1,[ ])
+  dnl ],[
+  dnl   AC_MSG_ERROR([wrong php_design_patterns lib version or lib not found])
+  dnl ],[
+  dnl   -L$PHP_DESIGN_PATTERNS_DIR/$PHP_LIBDIR -lm
+  dnl ])
+  dnl
+  dnl PHP_SUBST(PHP_DESIGN_PATTERNS_SHARED_LIBADD)
+
+  PHP_NEW_EXTENSION(php_design_patterns, php_design_patterns.c, $ext_shared,, -DZEND_ENABLE_STATIC_TSRMLS_CACHE=1)
+fi
---
+++ php_design_patterns.c
@@ -0,0 +1,127 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 7                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2017 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:                                                              |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+#include "php_ini.h"
+#include "ext/standard/info.h"
+#include "php_php_design_patterns.h"
+
+
+/* {{{ PHP_MINIT_FUNCTION
+ */
+PHP_MINIT_FUNCTION(php_design_patterns)
+{
+	/* If you have INI entries, uncomment these lines
+	REGISTER_INI_ENTRIES();
+	*/
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MSHUTDOWN_FUNCTION
+ */
+PHP_MSHUTDOWN_FUNCTION(php_design_patterns)
+{
+	/* uncomment this line if you have INI entries
+	UNREGISTER_INI_ENTRIES();
+	*/
+	return SUCCESS;
+}
+/* }}} */
+
+/* Remove if there's nothing to do at request start */
+/* {{{ PHP_RINIT_FUNCTION
+ */
+PHP_RINIT_FUNCTION(php_design_patterns)
+{
+#if defined(COMPILE_DL_PHP_DESIGN_PATTERNS) && defined(ZTS)
+	ZEND_TSRMLS_CACHE_UPDATE();
+#endif
+	return SUCCESS;
+}
+/* }}} */
+
+/* Remove if there's nothing to do at request end */
+/* {{{ PHP_RSHUTDOWN_FUNCTION
+ */
+PHP_RSHUTDOWN_FUNCTION(php_design_patterns)
+{
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MINFO_FUNCTION
+ */
+PHP_MINFO_FUNCTION(php_design_patterns)
+{
+	php_info_print_table_start();
+	php_info_print_table_header(2, "php_design_patterns support", "enabled");
+	php_info_print_table_end();
+
+	/* Remove comments if you have entries in php.ini
+	DISPLAY_INI_ENTRIES();
+	*/
+}
+/* }}} */
+
+/* {{{ php_design_patterns_functions[]
+ *
+ * Every user visible function must have an entry in php_design_patterns_functions[].
+ */
+const zend_function_entry php_design_patterns_functions[] = {
+	PHP_FE_END	/* Must be the last line in php_design_patterns_functions[] */
+};
+/* }}} */
+
+/* {{{ php_design_patterns_module_entry
+ */
+zend_module_entry php_design_patterns_module_entry = {
+	STANDARD_MODULE_HEADER,
+	"php_design_patterns",
+	php_design_patterns_functions,
+	PHP_MINIT(php_design_patterns),
+	PHP_MSHUTDOWN(php_design_patterns),
+	PHP_RINIT(php_design_patterns),		/* Replace with NULL if there's nothing to do at request start */
+	PHP_RSHUTDOWN(php_design_patterns),	/* Replace with NULL if there's nothing to do at request end */
+	PHP_MINFO(php_design_patterns),
+	PHP_PHP_DESIGN_PATTERNS_VERSION,
+	STANDARD_MODULE_PROPERTIES
+};
+/* }}} */
+
+#ifdef COMPILE_DL_PHP_DESIGN_PATTERNS
+#ifdef ZTS
+ZEND_TSRMLS_CACHE_DEFINE()
+#endif
+ZEND_GET_MODULE(php_design_patterns)
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
