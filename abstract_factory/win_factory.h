50ec3444c82c2a7ad0c9314c95eb802ff62b8880
Laputa <shinemotec@gmail.com>
2017-04-22 22:32:28 +0800

    unresolve make test core dump

--- abstract_factory/win_factory.h
+++ abstract_factory/win_factory.h
@@ -1,7 +1,7 @@
 #ifndef WIN_FACTROY_H
 #define WIN_FACTROY_H
 PHP_METHOD(win_factory, CreateButton);
-PHP_METHOD(win_factory, CreateButton);
+PHP_METHOD(win_factory, CreateBorder);
 PHP_DESIGN_STARTUP_FUNCTION(win_factory);
 #endif
 
