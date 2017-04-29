f235cdf3257c1f55122f23c9aeb20f3620af4cd3
Laputa <shinemotec@gmail.com>
2017-04-30 01:59:59 +0800

    init DI patterns

--- dependencyInjection/database_connection.h
+++ dependencyInjection/database_connection.h
@@ -1,7 +1,7 @@
 #ifndef DATABASE_CONNECTION_H
 #define DATABASE_CONNECTION_H
-zend_class_entry * database_configuration_ce;
-PHP_METHOD(database_configuration, __construct);
-PHP_METHOD(database_configuration, getDsn);
-PHP_DESIGN_STARTUP_FUNCTION(database_configuration);
+zend_class_entry * database_connection_ce;
+PHP_METHOD(database_connection, __construct);
+PHP_METHOD(database_connection, getDsn);
+PHP_DESIGN_STARTUP_FUNCTION(database_connection);
 #endif
